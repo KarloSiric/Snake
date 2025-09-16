f4adbb1a26e2401956d377e646330dccca580b18
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-09-16 22:22:27 +0200

    Added .gitignore and some simple SDL2 stuff for initializing the SDL window

--- src/snake.c
+++ src/snake.c
@@ -2,15 +2,46 @@
 * @Author: karlosiric
 * @Date:   2025-09-16 21:36:13
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-09-16 21:53:47
+* @Last Modified time: 2025-09-16 22:20:15
 */
 
 
 #include <stdio.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, char const *argv[])
+
+
+
+
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 768;
+
+
+
+
+
+int main( int argc, char const *argv[] )
 {
+
+    SDL_Window *window = NULL;
+   
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) 
+    {
+
+        printf( "SDL Could not initialize! SDL Error: %s\n", SDL_GetError() );
+
+    }
+
     
+
+
+
+
+
     return 0;
 }
 
