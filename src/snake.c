fc655cc66dee1f49680a10bb404ea91cf0d85dc8
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-09-16 21:54:15 +0200

    init commit

--- src/snake.c
+++ src/snake.c
@@ -2,10 +2,15 @@
 * @Author: karlosiric
 * @Date:   2025-09-16 21:36:13
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-09-16 21:38:11
+* @Last Modified time: 2025-09-16 21:53:47
 */
 
 
 #include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
 
