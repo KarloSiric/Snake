f0f6db4fe74d35c189bde2c6a275337493ab962d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-10-03 14:40:38 +0200

    Started working on snake step by step line by line

--- src/game_config/config.h
+++ src/game_config/config.h
@@ -2,6 +2,11 @@
 #define GAME_CONFIG_H 
 
 
+#define ASPECT_RATIO ((float)(16.0f / 9.0f))
+#define WIDTH ((int)1400)
+#define HEIGHT ((int)(WIDTH / ASPECT_RATIO))
+
+
 
 
 
--- src/snake.c
+++ src/snake.c
@@ -2,25 +2,17 @@
 * @Author: karlosiric
 * @Date:   2025-09-16 21:36:13
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-09-16 22:52:54
+* @Last Modified time: 2025-09-17 09:40:22
 */
 
-
+#include "game_config/config.h"
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL.h>
-
-
-
-
-
-const int SCREEN_WIDTH = 1024;
-const int SCREEN_HEIGHT = 768;
-
-
 
 
 
@@ -31,11 +23,40 @@ int main( int argc, char const *argv[] )
    
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) 
     {
-
         printf( "SDL Could not initialize! SDL Error: %s\n", SDL_GetError() );
-
     }
+
+    window = SDL_CreateWindow("Classic Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                             WIDTH, HEIGHT, 0);
+
+    SDL_Delay(5000);
     
+
+
+
+
+}
+
+    
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
+
     
 
 
